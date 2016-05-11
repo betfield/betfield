a22de99ca49a4ca16dd42af15dec35e81c7c7b17
sur5ive <sur5ive@laptop>
2016-05-12 00:32:53 +0300

    Predictions table support for reactive changes

--- client/templates/views/predictions/predictions-table.js
+++ client/templates/views/predictions/predictions-table.js
@@ -19,83 +19,9 @@ Template.predictionsTable.onRendered(function(){
     }
     
     // Get data for Predictions table
-    initTable(userId);
+    //initTable(userId);
 
 });
-
-
-/*
-Template.predictionsTable.helpers({
-    getGroupSelected: function() {
-        return Template.instance().fixtures.get('groupSelected');
-    },
-});
-*/
-function initTable(userId) {
-    var fixtures = Predictions.find({"fixture.group": Template.instance().data}).fetch();
-    if (fixtures && fixtures.length) {
-        var table = document.getElementById("predictions");
-        // clear existing values from table
-        
-        var tbody = document.createElement('tbody');
-        tbody.setAttribute("id","pred-body");
-        
-        var date, home, vs, away, group, score;
-        var imgHome, imgAway;
-        var tr;
-        
-        fixtures.forEach(function (f){
-            tr = document.createElement('tr');
-            
-            date = document.createElement('td');
-            home = document.createElement('td');
-            vs = document.createElement('td');
-            away = document.createElement('td');
-            group = document.createElement('td');
-            score = document.createElement('td');
-            
-            imgHome = document.createElement("img");
-            imgAway = document.createElement("img");
-            scoreHome = document.createElement("input");
-            scoreAway = document.createElement("input");
-            
-            date.setAttribute("class", "bf-table-date");
-            home.setAttribute("class", "bf-table-home");
-            vs.setAttribute("class", "bf-table-vs");
-            away.setAttribute("class", "bf-table-away");
-            group.setAttribute("class", "bf-table-group");
-            score.setAttribute("class", "bf-table-score");
-            
-            setAttributes(scoreHome, {"value": f.fixture.result.homeGoals, "maxlength": 2, "type": "number", "min": 0, "max": 99, "class": "input-no-spinner"});
-            setAttributes(scoreAway, {"value": f.fixture.result.awayGoals, "maxlength": 2, "type": "number", "min": 0, "max": 99, "class": "input-no-spinner"});
-            
-            date.innerHTML = f.fixture.date + " @ " + f.fixture.time;
-            home.innerHTML = f.fixture.home_team.name;
-            away.innerHTML = f.fixture.away_team.name;
-            group.innerHTML = f.fixture.group;
-            score.innerHTML = scoreHome.outerHTML + " : " + scoreAway.outerHTML;
-            
-            imgHome.src = Meteor.settings.public.FOLDER_FLAGS + String(f.fixture.home_team.code).toLowerCase() + ".png";
-            imgAway.src = Meteor.settings.public.FOLDER_FLAGS + String(f.fixture.away_team.code).toLowerCase() + ".png";
-            
-            vs.innerHTML = imgHome.outerHTML + "vs" + imgAway.outerHTML;
-            
-            tr.appendChild(date);
-            tr.appendChild(home);
-            tr.appendChild(vs);
-            tr.appendChild(away);
-            tr.appendChild(group);
-            tr.appendChild(score);
-            
-            tbody.appendChild(tr);
-            
-        }); // end of for
-        table.replaceChild(tbody,document.getElementById("pred-body"));
-        
-    } else {
-        // TODO: Handle empty table
-    };
-}
 
 // Create empty prediction set for logged in user
 function initUserPredictions(userId) {
@@ -106,11 +32,4 @@ function initUserPredictions(userId) {
             Bert.alert( "Prediction created!", "success" );
         }
     });
-}
-
-// Set multiple attributes for HTML element
-function setAttributes(el, attrs) {
-    for(var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
 }
--- client/templates/views/predictions/predictions.js
+++ client/templates/views/predictions/predictions.js
@@ -13,17 +13,15 @@ Template.predictions.events({
         var currentTab = $( event.target ).closest( "" );
         
         if (id === "ALL") {
-            template.pred.set('groupSelected', $("#" + event.target.id).data("template"));
-            //{$in: ["A","B","C","D","E","F"]}
+            template.pred.set('groupSelected', {$in: ["A","B","C","D","E","F"]});
         } else {
-            template.pred.set('groupSelected', $("#" + event.target.id).data("template")); 
+            template.pred.set('groupSelected', id); 
         }
         
         $("#" + event.target.id).addClass("fc-state-active").siblings().removeClass("fc-state-active");
         console.log( "eventclick: ", template.pred.get("groupSelected") );
         
         return template.pred.get("groupSelected");
-        //return initTable(this.userId);
     }
 });
 
@@ -32,21 +30,21 @@ Template.predictions.helpers({
         return Template.instance().pred.get("groupSelected");
     },
     predictionsData: function() {
-        
-        return Template.instance().pred.get("groupSelected");
-/*
-        var data = {
-            "A": "",
-            "B": "",
-            "C": "",
-            "D": "",
-            "E": "",
-            "F": "",
-            "ALL": ""
-        };
+        var fixtures = Predictions.find({"fixture.group": Template.instance().pred.get("groupSelected")}).fetch();
+		var i = 0;
+		
+		fixtures.forEach(function(f) {
+			
+			var homeTeamCode = String(f.fixture.home_team.code).toLowerCase();
+			var awayTeamCode = String(f.fixture.away_team.code).toLowerCase();
 
-        return { items: data[ tab ] };
-        */
+			fixtures[i].fixture.home_team.imgSrc = Meteor.settings.public.FOLDER_FLAGS + homeTeamCode + ".png";
+			fixtures[i].fixture.away_team.imgSrc = Meteor.settings.public.FOLDER_FLAGS + awayTeamCode + ".png";
+
+			i++;
+		});
+
+        return fixtures;
     }
 });
 
